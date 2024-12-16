f58783ea480bcbc00b64ee396dcd477077870a12
Maciej Tomaszewski <66778869+groomershop-mt@users.noreply.github.com>
2024-12-16 20:31:21 +0100

    Update OrderPlaceAfter.php

--- Observer/OrderPlaceAfter.php
+++ Observer/OrderPlaceAfter.php
@@ -33,6 +33,10 @@ class OrderPlaceAfter implements ObserverInterface
 	        $order = $objectManager->create('\Magento\Sales\Model\Order')->load($id);
 	        $q_id = $order->getQuoteId();
 	        $q = $quoteFactory->create()->load($q_id);
+		$ruch_destinationcode = $q->getData('ruch_destinationcode')
+		if($ruch_destinationcode === null || $ruch_destinationcode == '') {
+		    continue;
+		}
 		$comment = __('ORLEN Paczka: %1', $q->getData('ruch_destinationcode'));
 		$order->addStatusHistoryComment($comment)
                     ->setIsCustomerNotified(false)
